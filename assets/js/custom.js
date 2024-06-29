80dcb26eb59c632ac77b598c33621569652e25b8
agatha-sb <swagatabose27@gmail.com>
2024-06-29 10:59:35 +0530

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,211 @@
+
+
+// ************************************************* //
+// * +++++++++++++ 02. Header sticky & navbar ++++++++++++++ * //
+// ************************************************* //
+
+Fancybox.bind('[data-fancybox="gallery-1"]', {
+  caption: function (fancybox, slide) {
+    const figurecaption = slide.triggerEl?.querySelector(".tab-caption");
+    return figurecaption ? figurecaption.innerHTML : slide.caption || "";
+  },
+});
+Fancybox.bind('[data-fancybox="gallery-2"]', {
+  caption: function (fancybox, slide) {
+    const figurecaption = slide.triggerEl?.querySelector(".tab-caption");
+    return figurecaption ? figurecaption.innerHTML : slide.caption || "";
+  },
+});
+Fancybox.bind('[data-fancybox="gallery-3"]', {
+  caption: function (fancybox, slide) {
+    const figurecaption = slide.triggerEl?.querySelector(".tab-caption");
+    return figurecaption ? figurecaption.innerHTML : slide.caption || "";
+  },
+});
+
+
+var heroSwiper = new Swiper(".dCup-hero__swiper", {
+  slidesPerView: 1,
+  autoPlay: false,
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+var mensSquadSwiper = new Swiper(".mk2-squad__kings-swiper", {
+  autoPlay: false,
+  spaceBetween: "15px",
+  grid: {
+    rows: 2,
+    fill: "row",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 2.5
+    },
+    768: {
+      slidesPerView: 2.5
+    },
+    1024: {
+      slidesPerView: 4
+    }
+  }
+});
+
+var womensSquadSwiper = new Swiper(".mk2-squad__kueens-swiper", {
+  autoPlay: false,
+  spaceBetween: "15px",
+  loop: true,
+  grid: {
+    rows: 2,
+    fill: "row",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 2.5
+    },
+    768: {
+      slidesPerView: 2.5
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+var fixturesSwiper = new Swiper(".mk2-fixtures__matches-swiper", {
+  direction: "vertical",
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+$('.popup-youtube').magnificPopup({
+  type: 'iframe',
+  iframe: {
+    patterns: {
+      youtube: {
+        index: 'youtube.com/',
+        id: function (url) {
+          var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+          if (!m || !m[1]) return null;
+          return m[1];
+        },
+        src: '//www.youtube.com/embed/%id%?autoplay=1'
+      },
+      vimeo: {
+        index: 'vimeo.com/',
+        id: function (url) {
+          var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+          if (!m || !m[5]) return null;
+          return m[5];
+        },
+        src: '//player.vimeo.com/video/%id%?autoplay=1'
+      }
+    }
+  }
+});
+
+
+// *************************************** //
+// * ++++++++ Home Page Video ++++++++++ * //
+// ************************************** //
+
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    width: '100%',
+    height: '100%',
+    playerVars: {
+      autoplay: 1,
+      loop: 1,
+      controls: 0,
+      showinfo: 0,
+      autohide: 1,
+      modestbranding: 1,
+      mute: 1,
+      suggestedQuality: 'default',
+      origin: 'https://www.dublinseo.net'
+    },
+    videoId: document.getElementById('player').dataset.id,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+  player.mute();
+}
+
+var done = false;
+
+function onPlayerStateChange(e) {
+  var id = document.getElementById('player').dataset.id;
+
+  if (e.data === YT.PlayerState.ENDED) {
+    player.loadVideoById(id);
+  }
+}
---
+++ design/theme-color.md
@@ -0,0 +1,23 @@
+Red: #eb1f28
+Blue: #223d78
+Gold: #f0be5e
+Gold-shade: #af8031
+Blue-shade: #0c50a0
+Sky-blue: #7bc6ee
+
+
+1) https://campo.bold-themes.com/demo-01/home/soccer/
+2) https://topscorer.qodeinteractive.com/hockey/
+3) https://topscorer.qodeinteractive.com/league-standings/
+4) https://topscorer.qodeinteractive.com/match-schedule/
+5) https://preview.themeforest.net/item/campo-sport-club-and-team-wordpress-theme/full_screen_preview/42906867
+6) https://campo.bold-themes.com/demo-01/home/football/
+7) http://preview.themeforest.net/item/alteticos-football-sports-club-wordpress-theme/full_screen_preview/51416476
+8) https://preview.themeforest.net/item/topscorer-sports-wordpress-theme/full_screen_preview/26139531
+9) https://topscorer.qodeinteractive.com/rugby/
+10) https://topscorer.qodeinteractive.com/baseball/
+11) https://topscorer.qodeinteractive.com/baseball/#
+12) https://themeforest.net/item/fc-football-club-template-soccer-psd/9326478
+13) https://themeforest.net/item/vensica-fc-football-club-creative-psd-template/22001140
+14) https://themeforest.net/item/timrota-paintball-club-psd-template/38794883
+15) https://alchemists.dan-fisher.dev/soccer/_soccer_index.html#
